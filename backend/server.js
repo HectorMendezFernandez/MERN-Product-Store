3176c52fb65d9515b271212f7b1718eda035d0b3
HectorMendezCIP <hector.mendez.fernandez@est.una.ac.cr>
2024-10-03 21:34:54 -0600

    Add Connection to MongoDB
    - add in the .env file the mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+//get config from .env file
+dotenv.config();
+
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+console.log(process.env.MONGO_URI);
 //run the server
 app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+  console.log('Server is running on port 5000 -> http://localhost:5000');
 });
