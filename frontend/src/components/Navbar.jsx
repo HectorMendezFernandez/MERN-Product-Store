8826f32f7437b2d2dd02c7023e82897d1cb526b7
HectorMendezCIP <hector.mendez.fernandez@est.una.ac.cr>
2024-10-08 01:06:59 -0600

    Add Navbar with create icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,41 @@
+import { Button, Container, Flex, HStack, Link, Text} from "@chakra-ui/react";
+import { FaPlusSquare } from 'react-icons/fa';
+
 export function Navbar() {
   return (
-    <div>
-        Navbar
-    </div>
-  )
-}   
+    <Container maxW="container.xl" bg="gray.100" p={4}>
+      <Flex
+        justifyContent="space-between"
+        h={16}
+        alignItems={"center"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <Link href="/">Product Store</Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link href="/create">
+            <Button colorScheme="blue">
+                <FaPlusSquare size={20} />
+            </Button>
+          </Link>
+          <Link href="/products">
+            <Button colorScheme="blue">Products</Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+}
 
-export default Navbar
+export default Navbar;
