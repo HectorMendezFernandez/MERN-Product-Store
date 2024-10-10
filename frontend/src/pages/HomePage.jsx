650efd005ba2f1d249826589b12cb7497b5b2bbc
HectorMendezCIP <hector.mendez.fernandez@est.una.ac.cr>
2024-10-09 22:10:30 -0600

    Update homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
+import { Container, Text, VStack } from "@chakra-ui/react"
+
 export function HomePage() {
   return (
-    <div> HomePage </div>
+    <Container maxW="container.x1" py={12}>
+      <VStack spacing={8}>
+        <Text 
+        fontSize={30} 
+        textAlign="center" 
+        bgGradient={"linear(to-r, cyan.400, blue.500)"}
+        bgClip="text"
+        >Welcome to the Home Page
+
+        </Text>
+      </VStack>
+    </Container>
+
+
   )
 }   
 
