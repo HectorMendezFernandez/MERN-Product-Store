959677bdc9d839b577f740258e4a05cbd0df8388
HectorMendezCIP <hector.mendez.fernandez@est.una.ac.cr>
2024-10-13 03:03:57 -0600

    ADD env changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import path from 'path';
 
 //get config from .env file
 dotenv.config();
@@ -10,10 +11,22 @@ const app = express();
 //get port from .env file or use 5000
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 //allow to accept json data in the body of request
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    }
+    );
+}
 //get request
 app.get('/', (req, res) => {
     res.send('Server is ready');
